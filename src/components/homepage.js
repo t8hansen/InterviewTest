7cf308c2235bc2591d95106a53e0fea60a1e8bd4
Thomas Hansen <t8hansen@gmail.com>
2021-03-24 17:09:47 -0400

    Added the sorting

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 
 import {information} from './const/preset.js'
@@ -6,11 +6,77 @@ import {timeConverter} from './const/const.js'
 
 
 export const Homepage = () => {
-    const allEarthQuakesData = information.data
+    const allEarthQuakesData = information.data.features
+    const title = information.data.metadata.title
+    const [earthquakeData, setEarthquakeData] = useState(allEarthQuakesData);
+    const [placeAscSort, setPlaceAscSort] = useState(false)
+    const [magAscSort, setMagAscSort] = useState(false)
+    const [timeAscSort, setTimeAscSort] = useState(false)
 
-    const title = allEarthQuakesData.metadata.title
+    //Sorting Functions
+    const sortByPlace = () => {
+        let sortedEarthquakeData
+        if (!placeAscSort) {
+            sortedEarthquakeData = allEarthQuakesData.sort((a,b)=> {
+                if (a.properties.place < b.properties.place){
+                    return -1
+                } else {
+                    return 1
+                }
+            })
+        } else {
+            sortedEarthquakeData = allEarthQuakesData.sort((a,b)=> {
+                if (a.properties.place > b.properties.place){
+                    return -1
+                } else {
+                    return 1
+                }
+            })
+        }
+        setPlaceAscSort(!placeAscSort)
+        return sortedEarthquakeData
+        
+    }    
+    const sortByMagnitude = () => {
+        let sortedEarthquakeData
+        if (!magAscSort) {
+            sortedEarthquakeData = allEarthQuakesData.sort((a,b)=> {
+                return a.properties.mag - b.properties.mag
+            })
+        } else {
+            sortedEarthquakeData = allEarthQuakesData.sort((a,b)=> {
+                return b.properties.mag - a.properties.mag
+            })
+        }
+        setMagAscSort(!magAscSort)
+        return sortedEarthquakeData        
+    }
+    const sortByTime = () => {
+        let sortedEarthquakeData
+        if (!timeAscSort) {
+            sortedEarthquakeData = allEarthQuakesData.sort((a,b)=> {
+                return a.properties.time - b.properties.time
+            })
+        } else {
+            sortedEarthquakeData = allEarthQuakesData.sort((a,b)=> {
+                return b.properties.time - a.properties.time
+            })
+        }
+        setTimeAscSort(!timeAscSort)
+        return sortedEarthquakeData        
+    }
+    const sortEarthQuakeData = (column) => {
+        if (column === 'Place') {
+            setEarthquakeData([...sortByPlace()])
+        } else if (column === 'Time') {
+            setEarthquakeData([...sortByTime()])
+        } else if (column === 'Magnitude') {
+            setEarthquakeData([...sortByMagnitude()])
+        }
+    }
 
-    const earthquakeGrid = allEarthQuakesData.features.map((earthquake) => {
+    
+    const earthquakeGrid = earthquakeData.map((earthquake) => {
         return(
         <div
             key = {earthquake.id}
@@ -28,6 +94,8 @@ export const Homepage = () => {
             </div>
         </div>)
     })
+    
+    
 
     return(
         <div>
@@ -41,16 +109,25 @@ export const Homepage = () => {
                     style={{display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', width: '60%'}}
                 >
                     <h3>
-                        Place
+                        <button onClick = {()=>sortEarthQuakeData('Place')}
+                        style={{backgroundColor:'transparent', borderColor:'transparent'}}>
+                            Place
+                        </button>
                     </h3>
                     <h3>
+                        <button onClick = {()=>sortEarthQuakeData('Magnitude')}
+                        style={{backgroundColor:'transparent', borderColor:'transparent'}}>
                         Magnitude
+                        </button>
                     </h3>
-                    <h3>
+                    <h3>                        
+                        <button onClick = {()=>sortEarthQuakeData('Time')}
+                        style={{backgroundColor:'transparent', borderColor:'transparent'}}>
                         Time
+                        </button>
                     </h3>
                 </div>
-                 {earthquakeGrid}
+                {earthquakeGrid}
             </center>         
         </div>
     )
